050bd6544d0bead6e5b2668448c30160e97b9e57
Alejandro Perez <alejok0@think-less.ipv6.infinitum.net.mx>
2025-03-04 12:45:30 -0600

    Inicialización del backend con Spring Boot - Agrega configuración de Maven y wrapper - Incluye la clase principal AlumnoApplication - Configura properties iniciales - Añade pruebas base en AlumnoApplicationTests

---
+++ src/main/java/com/alumnos/itch/alumno/AlumnoApplication.java
@@ -0,0 +1,13 @@
+package com.alumnos.itch.alumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnoApplication.class, args);
+	}
+
+}
